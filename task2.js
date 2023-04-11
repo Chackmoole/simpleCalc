f0177592000b2ab67e6ad791d56555fca1e4c4e9
Chackmoole <cebep-21@mail.ru>
2023-04-11 18:43:53 +0300

    task2 fix.

--- task2.js
+++ task2.js
@@ -9,7 +9,7 @@ const arr = [5, 3, 8, 1];
 
 const filterRange = (arr, a, b) => {
     const filtredArr = [];
-    arr.map(item => item >= a && item <= b ? filtredArr.push(item) : console.log(item));
+    arr.forEach(item => item >= a && item <= b ? filtredArr.push(item) : console.log(item));
     return filtredArr;
 }
 
